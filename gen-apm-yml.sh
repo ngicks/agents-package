703fb96cb5730a8c364239f82f94d473584e796d
ngicks <yknt.bsl@gmail.com>
2026-07-24 08:25:27 +0900

    ✨: exclude packages for fallback

--- gen-apm-yml.sh
+++ gen-apm-yml.sh
@@ -2,6 +2,6 @@
 
 pushd $(dirname $0) > /dev/null 2>&1
 
-moon run ./.tool/cmd/main -- gen-apm-yml "$@"
+moon run ./.tool/cmd/main -- gen-apm-yml --exclude '**gorimpots' --exclude '**nix-*' --exclude '**nggoal' "$@"
 
 popd > /dev/null 2>&1
